a72e2652cebcff7020136d471c2e61830e93fd2a
ABI DAVID LIVING-STONE <abidavestone@gmail.com>
2023-03-27 11:51:04 -0700

    rev holberton

--- 0x05-pointers_arrays_strings/main_0.c
+++ 0x05-pointers_arrays_strings/main_0.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include <stdio.h>
+
+void print_rev(char *);
 
 /**
- * main - check the code
- *
+ * main - check the code for Holberton School students.
  * Return: Always 0.
  */
-
 int main(void)
 {
 char *str;
